ad52aeb614caeb569704959e2e611c67d7a1f0e8
Aleksander Davydiuk <mictopibhe@gmail.com>
2024-07-21 11:06:46 +0200

    Added implementation PersonDAO

---
+++ src/main/java/pl/davidduke/dao/PersonDAO.java
@@ -0,0 +1,25 @@
+package pl.davidduke.dao;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import pl.davidduke.model.Person;
+
+import java.util.List;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PersonDAO {
+    List<Person> people = List.of(
+            new Person(1, "Tom"),
+            new Person(2, "Bob"),
+            new Person(3, "Jack"),
+            new Person(4, "Jane")
+    );
+
+    public List<Person> getPeople() {
+        return people;
+    }
+
+    public Person getPerson(int id) {
+        return people.stream().filter(person -> person.getId() == id).findFirst().orElse(null);
+    }
+}
